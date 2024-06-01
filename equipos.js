caf863391c7bb9d4cbf2a24b2f41ceba5c7dc542
alonso <amuc23@gmail.com>
2024-05-31 20:35:00 -0400

    first commit

---
+++ equipos.js
@@ -0,0 +1,32 @@
+
+
+const equipos = [
+    
+        {
+            "id": 1,
+            "nombre": "Colo Colo",
+            "pais": "chile",
+            "imagen": "hhttps://i.pinimg.com/originals/7c/d0/35/7cd035347996101c9c8c768a27b138a5.jpg"
+        },
+        {
+            "id": 2,
+            "nombre": "san lorenzo",
+            "pais": "argentina",
+            "imagen": "https://upload.wikimedia.org/wikipedia/commons/6/62/Escudo_del_Club_Atl%C3%A9tico_San_Lorenzo_de_Almagro.png"
+        },
+        {
+            "id": 3,
+            "nombre": "vasco de gama",
+            "pais": "brasil",
+            "imagen": "https://i.pinimg.com/564x/9d/85/93/9d859311311dbfefb2b8f86f4b6ea758.jpg"
+        },
+        {
+            "id": 4,
+            "nombre": "cienciano",
+            "pais": "peru",
+            "imagen": "https://upload.wikimedia.org/wikipedia/commons/4/41/Escudo_Cienciano.png"
+        }
+    ];
+module.exports = {
+    equipos
+}
